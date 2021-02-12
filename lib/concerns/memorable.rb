768f98fe2f82f7294f8167488a6badd4a46b8159
rtyenriques <tooshopboys@gmail.com>
2021-02-11 22:39:59 -0800

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,21 @@
+module Memorable
+  module ClassMethods
+def reset_all
+  self.all.clear
+end
+
+def count
+  self.all.count
+end
+end
+module InstanceMethods
+  def initialize
+      self.class.all << self
+  end
+  def to_param
+      name.downcase.gsub(' ', '-')
+    end
+end
+
+
+end
